4a7821363f5f70d72bcf1da383ee22a25a60e3a2
Wu, Ruth <ruth.wu@accenture.com>
2018-03-10 16:07:11 -0800

    proptypes

--- views/index.jsx
+++ views/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-    
+
+    import React from 'react';
+    import PropTypes from 'prop-types';    
     export default class TodoBox extends React.Component {
-        render() {
+      render() {
             return (
                 <div className="todoBox">
                     <h1>Todos</h1>
@@ -13,20 +14,36 @@ import React from 'react';
     }
     
     class TodoList extends React.Component {
+        render() {
+            return (
+                <div className="todoList">
+                    <table style={{border: "2px solid black;"}}>
+                        <tbody>
+                        <Todo title="Shopping">Milk</Todo>
+                        <Todo title="Hair cut">13:00</Todo>
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
+    }
+    
+    class Todo extends React.Component {
       render(){
 	return(
-	<div className="todoList">
-	 I am a TodoList.
-	</div>);
+	<tr>
+	<td style={{border:"1px solid black;"}}>{this.props.title}</td>
+	<td style={{border:"1px solid black;"}}>{this.props.children}</td>
+	</tr>
+	);
 	}
     }
     
     class TodoForm extends React.Component {
-      render(){
-	return(
-	<div className="todoForm">
-	I am a TodoForm.
-	</div>);
+        render(){
+	return( 
+	 <div className="todoForm">
+     		 I am a TodoForm.
+    	 </div>);	
 	}
     }
-
